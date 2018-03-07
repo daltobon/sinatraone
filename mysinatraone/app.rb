f5e9bbfac374a04ad1fc21e6a0b50e5ac4645d09
daltobon <daniel18.tobon@gmail.com>
2018-03-06 20:53:48 -0500

    Cambios en mysinatraone

--- mysinatraone/app.rb
+++ mysinatraone/app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'faker'
 get '/' do  #ruta de about, home,
-  
+  @b = Array.new
+    6.times.each do
+      @b << Faker::DragonBall.character
+    end
+    @c = Array.new
+      6.times.each do
+        @c << Faker::Dune.quote
+      end
     erb :index
 end
