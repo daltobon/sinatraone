94da1bbce9d3f1dcfc06ff2389fee94f38a1eea4
daltobon <daniel18.tobon@gmail.com>
2018-03-06 20:51:22 -0500

    Aprendiendo HTTP y sintra

--- firstapp/api.rb
+++ firstapp/api.rb
@@ -1,17 +1,19 @@
 require 'sinatra'
 require 'faker'
+require 'pry'
 
 get '/' do
     @nombre = Faker::DragonBall.character
-
+    @hello = "Hola #{params[:name]}"
     erb :index
 end
 
 get '/about' do
     @nosotros = Faker::Name.name
-
+    binding.pry
     erb :about
 end
+
 
 get '/contact' do
 
